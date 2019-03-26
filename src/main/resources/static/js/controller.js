728e79ecdef01098d52fa3baf4b20c26daca9f1e
Plamen Peev <plamen@peev.dev>
2019-03-27 01:16:17 +0200

    Introduce OnsenUI2

---
+++ src/main/resources/static/js/controller.js
@@ -0,0 +1,34 @@
+document.addEventListener("init", function(event) {
+  var page = event.target;
+  if (page.id === "page1") {
+    page.querySelector("#paste-button").onclick = function() {
+      paste();
+    };
+  } else if (page.id === "page2") {
+    page.querySelector("ons-toolbar .center").innerHTML = page.data.title;
+    page.querySelector("#responsediv").innerHTML = page.data.response;
+  }
+});
+
+function paste() {
+  var formTitle = document.getElementById("paste-title").value
+  var formContent = document.getElementById("paste-content").value
+  var payload = { title: formTitle, content: formContent }
+
+  fetch("/paste", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    },
+    body: JSON.stringify(payload)
+  })
+  .then(response => {
+    return response.json()
+  })
+.then(json => {
+    document.querySelector("#myNavigator").pushPage("page2.html", { data: { title: "Response:", response: JSON.stringify(json) } })
+  }).catch(error => {
+    console.log(error),
+    ons.notification.alert("Failed to submit the paste!");
+});
+}
